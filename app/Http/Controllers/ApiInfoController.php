ab309dc41d049e5051198bfd57e15db9a0c27819
Taskinur Rahman <taskinur.rahman2014@gmail.com>
2021-08-19 11:05:40 +0600

    added apiinfo controller

---
+++ app/Http/Controllers/ApiInfoController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Response;
+
+class ApiInfoController extends Controller
+{
+    const SERVICE_NAME = 'NISE-3 Youth Management API Service';
+    const SERVICE_VERSION = 'V1';
+
+    public function apiInfo(): JsonResponse
+    {
+        $response = [
+            'service_name' => self::SERVICE_NAME,
+            'service_version' => self::SERVICE_VERSION,
+            'lumen_version' => App::version(),
+            'module_list' => [
+                'Youth'
+            ],
+            'description' => [
+                'It is a youth management api service that manages the youths'
+            ]
+        ];
+        return Response::json($response, JsonResponse::HTTP_OK);
+    }
+}
