6ed9b7467675b386fdcd89aaadca3a06b037d1ec
rrahul <rahulbgc21@gmail.com>
2021-10-05 11:08:25 +0600

    helper client url refactoring

--- app/Helpers/functions/helpers.php
+++ app/Helpers/functions/helpers.php
@@ -1,29 +1,31 @@
 <?php
 
+use App\Models\BaseModel;
+
 if (!function_exists("clientUrl")) {
     function clientUrl($type)
     {
         $config = config("httpclientendpoint");
 
         if (!in_array(request()->getHost(), ['localhost', '127.0.0.1'])) {
-            if ($type == "CORE") {
+            if ($type == BaseModel::CORE_CLIENT_URL_TYPE) {
                 return env("IS_DEVELOPMENT_MOOD",false) ? $config["core"]["dev"] : $config["core"]["prod"];
-            } elseif ($type == "ORGANIZATION") {
+            } elseif ($type == BaseModel::ORGANIZATION_CLIENT_URL_TYPE) {
                 return env("IS_DEVELOPMENT_MOOD",false) ? $config["organization"]["dev"] : $config["organization"]["prod"];
-            } elseif ($type == "INSTITUTE") {
+            } elseif ($type == BaseModel::INSTITUTE_URL_CLIENT_TYPE) {
                 return env("IS_DEVELOPMENT_MOOD",false) ? $config["institute"]["dev"] : $config["institute"]["prod"];
-            } elseif ($type == "IDP_SERVER") {
+            } elseif ($type == BaseModel::IDP_SERVER_CLIENT_URL_TYPE) {
                 return env("IS_DEVELOPMENT_MOOD",false) ? $config["idp_server"]["dev"] : $config["idp_server"]["prod"];
             }
 
         } else {
-            if ($type == "CORE") {
+            if ($type == BaseModel::CORE_CLIENT_URL_TYPE) {
                 return $config["core"]["local"];
-            } elseif ($type == "ORGANIZATION") {
+            } elseif ($type == BaseModel::ORGANIZATION_CLIENT_URL_TYPE) {
                 return $config["organization"]["local"];
-            } elseif ($type == "INSTITUTE") {
+            } elseif ($type == BaseModel::INSTITUTE_URL_CLIENT_TYPE) {
                 return $config["institute"]["local"];
-            } elseif ($type == "IDP_SERVER") {
+            } elseif ($type == BaseModel::IDP_SERVER_CLIENT_URL_TYPE) {
                 return env("IS_DEVELOPMENT_MOOD",false) ? $config["idp_server"]["dev"] : $config["idp_server"]["prod"];
             }
         }
