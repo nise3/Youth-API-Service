ecfc7268ab37e1576eaedd2bf996cc7208531f34
taskinur rahman <taskinur.rahman2014@gmail.com>
2021-10-07 11:01:03 +0600

    updated skill route

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Helpers\Classes\CustomRouter;
 use Laravel\Lumen\Routing\Router;
 
 
-$router->get('/', function (){
+$router->get('/', function () {
     return "Call api youth success";
 });
 
@@ -19,9 +19,9 @@ $customRouter = function (string $as = '') use ($router) {
 
 $router->group(['prefix' => 'api/v1', 'as' => 'api.v1'], function () use ($router, $customRouter) {
     $router->get('/', ['as' => 'api-info', 'uses' => 'ApiInfoController@apiInfo']);
+    $customRouter()->resourceRoute('skills', 'SkillController')->render();
     $customRouter()->resourceRoute('youths', 'YouthController')->render();
     $customRouter()->resourceRoute('youth-portfolios', 'PortfolioController')->render();
-    $customRouter()->resourceRoute('youth-skills', 'SkillController')->render();
     $customRouter()->resourceRoute('youth-job-experience', 'JobExperienceController')->render();
     $customRouter()->resourceRoute('youth-references', 'ReferenceController')->render();
     $customRouter()->resourceRoute('youth-languages-proficiencies', 'LanguagesProficiencyController')->render();
@@ -29,19 +29,19 @@ $router->group(['prefix' => 'api/v1', 'as' => 'api.v1'], function () use ($route
     $customRouter()->resourceRoute('youth-educations', 'EducationController')->render();
 
     /** youth registration */
-    $router->post('youth-registration', ["as"=>"youth.registration","uses"=>"YouthProfileController@youthRegistration"]);
+    $router->post('youth-registration', ["as" => "youth.registration", "uses" => "YouthProfileController@youthRegistration"]);
 
     /** youth profile */
-    $router->get('youth-profile', ["as"=>"youth-profile.get-profile","uses"=>"YouthProfileController@getYouthProfile", "middleware" => 'auth']);
-    $router->put('youth-personal-info-update', ["as"=>"youth-profile.update","uses"=>"YouthProfileController@youthProfileUpdate", "middleware" => 'auth']);
-    $router->put('youth-change-freelance-status', ["as"=>"youth-profile.youth-change-freelance-status","uses"=>"YouthProfileController@setFreelanceStatus", "middleware" => 'auth']);
+    $router->get('youth-profile', ["as" => "youth-profile.get-profile", "uses" => "YouthProfileController@getYouthProfile", "middleware" => 'auth']);
+    $router->put('youth-personal-info-update', ["as" => "youth-profile.update", "uses" => "YouthProfileController@youthProfileUpdate", "middleware" => 'auth']);
+    $router->put('youth-change-freelance-status', ["as" => "youth-profile.youth-change-freelance-status", "uses" => "YouthProfileController@setFreelanceStatus", "middleware" => 'auth']);
 
     /** youth verification */
-    $router->post('youth-profile-verification', ["as"=>"youth-profile.verify","uses"=>"YouthProfileController@youthVerification"]);
-    $router->post('youth-resend-verification-code', ["as"=>"youth-profile.youth-resend-verify-code","uses"=>"YouthProfileController@resendVerificationCode"]);
+    $router->post('youth-profile-verification', ["as" => "youth-profile.verify", "uses" => "YouthProfileController@youthVerification"]);
+    $router->post('youth-resend-verification-code', ["as" => "youth-profile.youth-resend-verify-code", "uses" => "YouthProfileController@resendVerificationCode"]);
 
     /** freelance corner */
-    $router->get('freelancers', ["as"=>"freelancers.get-all-freelancers","uses"=>"FreelanceController@getAllFreelancers"]);
+    $router->get('freelancers', ["as" => "freelancers.get-all-freelancers", "uses" => "FreelanceController@getAllFreelancers"]);
 
 
 });
