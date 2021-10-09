210dab57998720c4d1eaba16d2c95ac5ed012acc
A.S.M. Mahmudul Hasan <asmmahmud@gmail.com>
2021-10-09 15:15:13 +0600

    fixing many to many relation between youth and skills

--- app/Models/PhysicalDisability.php
+++ app/Models/PhysicalDisability.php
@@ -3,14 +3,34 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Collection;
 
+/**
+ * Class Skill
+ * @package App\Models
+ * @property string title_en
+ * @property string title
+ * @property int | null description
+ * @property int row_status
+ * @property-read  Collection youths
+ */
 class PhysicalDisability extends BaseModel
 {
+    /**
+     * @var string[]
+     */
+    protected $guarded = ['id'];
 
+    /**
+     * @var string[]
+     */
     protected $hidden = [
         "pivot"
     ];
 
+    /**
+     * @return BelongsToMany
+     */
     public function youths(): BelongsToMany
     {
         return $this->belongsToMany(Youth::class, 'youth_physical_disabilities');
