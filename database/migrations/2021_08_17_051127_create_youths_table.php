f58359eb03aa3392ddcf817dda2556865b0d96c9
A.S.M. Mahmudul Hasan <asmmahmud@gmail.com>
2021-10-09 10:44:05 +0600

    Youth tables Migrations, seeder, factory updated

--- database/migrations/2021_08_17_051127_create_youths_table.php
+++ database/migrations/2021_08_17_051127_create_youths_table.php
@@ -16,23 +16,28 @@ class CreateYouthsTable extends Migration
         Schema::create('youths', function (Blueprint $table) {
             $table->increments('id');
             $table->string("idp_user_id", 100)->nullable();
-            $table->string("is_freelance_profile", 100)->default(0);
-            $table->string("username", 100)->unique()->comment("username is either email or mobile");
-            $table->unsignedTinyInteger("user_name_type")->comment("1=>Email Type,2=>Mobile Number");
+            $table->string("is_freelance_profile", 100)
+                ->default(0);
+            $table->string("username", 100)->unique()
+                ->comment("username is either email or mobile");
+            $table->unsignedTinyInteger("user_name_type")
+                ->comment("1=>Email Type,2=>Mobile Number");
 
             $table->string('first_name', 500);
             $table->string('first_name_en', 500)->nullable();
             $table->string('last_name', 500);
             $table->string('last_name_en', 500)->nullable();
 
-            $table->unsignedTinyInteger('gender')->comment('1=>male,2=>female,3=>others');
+            $table->unsignedTinyInteger('gender')
+                ->comment('1=>male,2=>female,3=>others');
 
             $table->string('email', 191)->unique();
             $table->string('mobile', 20)->unique();
 
             $table->date('date_of_birth');
 
-            $table->unsignedTinyInteger('physical_disability_status')->comment('0=>No,1=>Yes')->default(0);
+            $table->unsignedTinyInteger('physical_disability_status')
+                ->comment('0=>No,1=>Yes')->default(0);
 
             $table->unsignedMediumInteger("loc_division_id");
             $table->unsignedMediumInteger("loc_district_id");
@@ -50,9 +55,12 @@ class CreateYouthsTable extends Migration
             $table->string('photo', 300)->nullable();
             $table->string('cv_path', 300)->nullable();
 
-            $table->string("verification_code", 10)->nullable()->comment('Email Or SMS verification code');
-            $table->dateTime("verification_code_sent_at")->nullable()->comment('Email Or SMS verification code sent at');
-            $table->dateTime("verification_code_verified_at")->nullable()->comment('Email Or SMS verification code verified at');
+            $table->string("verification_code", 10)->nullable()
+                ->comment('Email Or SMS verification code');
+            $table->dateTime("verification_code_sent_at")->nullable()
+                ->comment('Email Or SMS verification code sent at');
+            $table->dateTime("verification_code_verified_at")->nullable()
+                ->comment('Email Or SMS verification code verified at');
 
             $table->unsignedTinyInteger("row_status")->default(0);
             $table->timestamps();
