877cde3a469aaf6f3d8628677adfd15826044e7c
A.S.M. Mahmudul Hasan <asmmahmud@gmail.com>
2021-10-09 14:37:22 +0600

    fixing many to many relation between youth and physicaldisability

--- app/Models/PhysicalDisability.php
+++ app/Models/PhysicalDisability.php
@@ -2,12 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class PhysicalDisability extends Model
+class PhysicalDisability extends BaseModel
 {
 
     protected $hidden=[
         "pivot"
     ];
+
+    public function youths(): BelongsToMany
+    {
+        return $this->belongsToMany(Youth::class, 'youth_physical_disabilities');
+    }
 }
