0a3f610c072c046457603d6a6b0bc7c6405cd3ce
A.S.M. Mahmudul Hasan <asmmahmud@gmail.com>
2021-10-09 14:40:30 +0600

    fixing many to many relation between youth and skills

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * Class Skill
  * @package App\Models
  * @property string title_en
- * @property string title_bn
+ * @property string title
  * @property int | null description
  * @property int row_status
  */
@@ -21,6 +22,13 @@ class Skill extends BaseModel
      * @var string[]
      */
     protected $guarded = ['id'];
-
+    /**
+     * @var string[]
+     */
     protected $hidden = ["pivot"];
+
+    public function youths(): BelongsToMany
+    {
+        return $this->belongsToMany(Youth::class, 'youth_skills');
+    }
 }
