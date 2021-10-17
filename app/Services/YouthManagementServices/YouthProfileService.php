ec6c74447c2981b4da7733063e3d4240fdc415fc
rrahul <rahulbgc21@gmail.com>
2021-10-17 16:22:18 +0600

    youth registration stuff

--- app/Services/YouthManagementServices/YouthProfileService.php
+++ app/Services/YouthManagementServices/YouthProfileService.php
@@ -438,18 +438,12 @@ class YouthProfileService
             "last_name" => "required|string|min:2|max:500",
             "last_name_en" => "nullable|string|min:2|max:500",
             "loc_division_id" => [
-                "nullable",
-                Rule::requiredIf(function () use ($id) {
-                    return $id == null;
-                }),
+                "required",
                 "exists:loc_divisions,id",
                 "int"
             ],
             "loc_district_id" => [
-                "nullable",
-                Rule::requiredIf(function () use ($id) {
-                    return $id == null;
-                }),
+                "required",
                 "exists:loc_districts,id",
                 "int"
             ],
