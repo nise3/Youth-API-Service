df89bb51746dd50819fb355cf1797ac087b66b2e
A.S.M. Mahmudul Hasan <asmmahmud@gmail.com>
2021-10-18 22:24:31 +0600

    Extending MessageBag of Validation (Trying)

--- app/Extend/MessageBag.php
+++ app/Extend/MessageBag.php
@@ -3,16 +3,16 @@
 namespace App\Extend;
 
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\MessageBag;
+use Illuminate\Support\MessageBag as CoreMessageBag;
 use Illuminate\Support\Traits\ForwardsCalls;
 
-class MessageBagExtended
+class MessageBag
 {
     use ForwardsCalls;
 
-    protected MessageBag $messageBag;
+    protected CoreMessageBag $messageBag;
 
-    public function __construct(MessageBag $messageBag)
+    public function __construct(CoreMessageBag $messageBag)
     {
         $this->messageBag = $messageBag;
     }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,8 @@
 
 namespace App\Providers;
 
-use App\Extend\MessageBagExtended;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\MessageBag;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,10 +25,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-
-
-        $this->app->extend(MessageBag::class,  function (MessageBag $messageBag) {
-            return new MessageBagExtended($messageBag);
-        });
     }
 }
