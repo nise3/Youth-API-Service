eeb1ab87a4cb7db6207e23dcaf400a0e1e6b750a
rrahul <rahulbgc21@gmail.com>
2021-10-28 16:47:08 +0600

    exam degree seeder refactor

--- database/seeders/ExamDegreeSeeder.php
+++ database/seeders/ExamDegreeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\ExamDegree;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class ExamDegreeSeeder extends Seeder
 {
@@ -14,6 +15,9 @@ class ExamDegreeSeeder extends Seeder
      */
     public function run()
     {
+        Schema::disableForeignKeyConstraints();
+
+        ExamDegree::query()->truncate();
         $examDegrees = [
             [
                 "education_level_id" => 1,
@@ -124,24 +128,19 @@ class ExamDegreeSeeder extends Seeder
                 "title_en" => "Others"
             ],
             [
-                "education_level_id" => 5,
-                "code" => "DIPLOMA_ENG",
-                "title" => "Diploma in Engineering",
-                "title_en" => "Diploma in Engineering"
-            ],
-            [
                 "education_level_id" => 6,
                 "code" => "BSC",
-                "title" => "Bachelor of Science",
+                "title" => "ব্যাচেলর অফ সায়েন্স",
                 "title_en" => "Bachelor of Science"
             ],
             [
-                "education_level_id" => 7,
-                "code" => "MSC",
-                "title" => "Masters of Science",
-                "title_en" => "Masters of Science"
+                "education_level_id" => 6,
+                "code" => "BA",
+                "title" => "ব্যাচেলর অফ আর্টস",
+                "title_en" => "Bachelor of Arts"
             ]
         ];
         ExamDegree::insert($examDegrees);
+        Schema::enableForeignKeyConstraints();
     }
 }
