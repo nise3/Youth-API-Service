14642b7070bba4811b46fc49d5fa3c675a0f41de
rrahul <rahulbgc21@gmail.com>
2021-11-04 12:03:56 +0600

    youth registration refactor

--- app/Helpers/functions/helpers.php
+++ app/Helpers/functions/helpers.php
@@ -109,7 +109,7 @@ if (!function_exists("idpUserErrorMessage")) {
             case ResponseAlias::HTTP_CONFLICT:
             {
                 $errors['_response_status']['code'] = ResponseAlias::HTTP_CONFLICT;
-                $errors['_response_status']['message'] = "Username already exists.";
+                $errors['_response_status']['message'] = "Already exists";
                 return $errors;
             }
             case 0:
