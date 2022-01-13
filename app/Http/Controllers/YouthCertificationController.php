b83df5dd2797aa7a880a1a8e8222ad1c46c3d814
Md Tasmidur Rahman <tasmidurrahman@gmail.com>
2022-01-13 17:09:00 +0600

    send main notification off

--- app/Http/Controllers/YouthCertificationController.php
+++ app/Http/Controllers/YouthCertificationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\CourseEnrollment\CourseEnrollmentSuccessEvent;
 use App\Events\MailSendEvent;
 use App\Models\YouthCertification;
 use App\Services\YouthManagementServices\YouthCertificationService;
