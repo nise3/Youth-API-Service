177ee2250cc03c539213283caf418edf210a58f1
ASM Mahmudul Hasan <asmmahmud@gmail.com>
2022-02-03 15:56:50 +0600

    Youth Code Generation Codebase updated and lock table schema updated

--- app/Models/YouthCodePessimisticLocking.php
+++ app/Models/YouthCodePessimisticLocking.php
@@ -2,8 +2,18 @@
 
 namespace App\Models;
 
-
+/**
+ * Class YouthCodePessimisticLocking
+ * @property  int last_incremental_value
+ */
 class YouthCodePessimisticLocking extends BaseModel
 {
-    protected $guarded = BaseModel::COMMON_GUARDED_FIELDS_SIMPLE;
+    public $timestamps = false;
+    public $incrementing = false;
+    protected $primaryKey = 'last_incremental_value';
+    protected $guarded = [];
+
+    protected $casts = [
+        'last_incremental_value' => 'integer'
+    ];
 }
