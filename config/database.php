37013d0a72b379ff2244413a6b102fc0898cbfc2
ASM Mahmudul Hasan <asmmahmud@gmail.com>
2022-02-08 18:43:33 +0600

    RELEASE = 'php artisan list' version: 2.0.14

--- config/database.php
+++ config/database.php
@@ -127,7 +127,7 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', 'youth_default'),
+            'database' => env('REDIS_DB', '0'),
             'read_write_timeout' => 60,
         ],
 
@@ -136,7 +136,7 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', 'youth_cache'),
+            'database' => env('REDIS_CACHE_DB', '1'),
             'read_write_timeout' => 60,
         ],
 
