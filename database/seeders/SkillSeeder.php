9063c4687007846f5081c86bbcc5e57895718544
ASM Mahmudul Hasan <asmmahmud@gmail.com>
2022-02-10 13:17:01 +0600

    RELEASE = 'php artisan db:seed --class=SkillSeeder' version: 2.1.5

--- database/seeders/SkillSeeder.php
+++ database/seeders/SkillSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Skill;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
@@ -11,166 +10,136 @@ class SkillSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
      * @return void
      */
     public function run()
     {
         Schema::disableForeignKeyConstraints();
-
         DB::table('skills')->truncate();
+
         $skills = [
             [
-                'id' => '1',
                 "title" => "টেকনিকাল রাইটিং",
                 "title_en" => "Technical Writing"
             ],
             [
-                'id' => '2',
                 "title" => "প্রজেক্ট ম্যানেজমেন্ট",
                 "title_en" => "Project Management"
             ],
             [
-                'id' => '3',
                 "title" => "কম্পিউটার প্রোগ্রামিং",
                 "title_en" => "Computer Programming"
             ],
             [
-                'id' => '4',
                 "title" => "ডাটা সাইন্স",
                 "title_en" => "Data Science"
             ],
             [
-                'id' => '5',
                 "title" => "একাউন্টিং",
                 "title_en" => "Accounting"
             ],
             [
-                'id' => '6',
                 "title" => "ইন্টারনেট",
                 "title_en" => "Internet"
             ],
             [
-                'id' => '7',
                 "title" => "ডাটা এন্ট্রি",
                 "title_en" => "Data Entry"
             ],
             [
-                'id' => '8',
                 "title" => "ডাটাবেজ অ্যাডমিনিস্ট্রেশন",
                 "title_en" => "Database Administration"
             ],
             [
-                'id' => '9',
                 "title" => "মাই.এস.কিউ.এল.",
                 "title_en" => "MySQL"
             ],
             [
-                'id' => '10',
                 "title" => "ওরাকল",
                 "title_en" => "Oracle"
             ],
             [
-                'id' => '11',
                 "title" => "উক্স ডিসাইন",
                 "title_en" => "UX Design"
             ],
             [
-                'id' => '12',
                 "title" => "ওয়েব ডিজাইন",
                 "title_en" => "Web Design"
             ],
             [
-                'id' => '13',
                 "title" => "কম্পিউটার প্রযুক্তি",
                 "title_en" => "Computer Technology"
             ],
             [
-                'id' => '14',
                 "title" => "বৈদ্যুতিক প্রযুক্তি",
                 "title_en" => "Electrical Technology"
             ],
             [
-                'id' => '15',
                 "title" => "যান্ত্রিক প্রযুক্তি",
                 "title_en" => "Mechanical Technology"
             ],
             [
-                'id' => '16',
                 "title" => "রাসায়নিক প্রযুক্তি",
                 "title_en" => "Chemical Technology"
             ],
             [
-                'id' => '17',
                 "title" => "ফার্মাসিউটিক্যাল প্রযুক্তি",
                 "title_en" => "Pharmaceutical Technology"
             ],
             [
-                'id' => '18',
                 "title" => "সেলাই",
                 "title_en" => "Sewing"
             ],
             [
-                'id' => '19',
                 "title" => "দর্জি",
                 "title_en" => "Tailor"
             ],
             [
-                'id' => '20',
                 "title" => "ইলেক্ট্রনিক্স মেকানিক",
                 "title_en" => "Electronics Mechanic"
             ],
             [
-                'id' => '21',
                 "title" => "রং মিস্ত্রি",
                 "title_en" => "Color Painter"
             ],
             [
-                'id' => '22',
                 "title" => "ড্রাইভিং",
                 "title_en" => "Driving"
             ],
             [
-                'id' => '23',
                 "title" => "প্লাম্বিং",
                 "title_en" => "Plumbing"
             ],
             [
-                'id' => '24',
                 "title" => "মোটর মেকানিক",
                 "title_en" => "Motor Mechanic"
             ],
             [
-                'id' => '25',
                 "title" => "পোল্ট্রি ফার্মিং",
                 "title_en" => "Poultry Farming"
             ],
             [
-                'id' => '26',
                 "title" => "মাছ চাষ",
                 "title_en" => "Fish Farming"
             ],
             [
-                'id' => '27',
                 "title" => "মেশিন লার্নিং",
                 "title_en" => "Machine Learning"
             ],
             [
-                'id' => '28',
                 "title" => "কম্পিউটার প্রশিক্ষণ",
                 "title_en" => "Computer Training"
             ],
             [
-                'id' => '29',
                 "title" => "সাংগঠনিক সংস্কৃতি",
                 "title_en" => "Organizational Culture"
             ],
             [
-                'id' => '30',
                 "title" => "ব্যবসায়িক লেখা",
                 "title_en" => "Business Writing"
             ],
             [
-                'id' => '31',
                 "title" => "কুকিং",
                 "title_en" => "Cooking"
             ],
@@ -228,7 +197,7 @@ class SkillSeeder extends Seeder
             ]
         ];
 
-        Skill::insert($skills);
+        DB::table('skills')->insert($skills);
 
         Schema::enableForeignKeyConstraints();
     }
