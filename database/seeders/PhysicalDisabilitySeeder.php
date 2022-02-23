8fd1a6412e7ea40294335dad5ccb5464766d7ff8
ASM Mahmudul Hasan <asmmahmud@gmail.com>
2022-02-23 17:22:51 +0600

    Seeder File Updated

--- database/seeders/PhysicalDisabilitySeeder.php
+++ database/seeders/PhysicalDisabilitySeeder.php
@@ -20,11 +20,11 @@ class PhysicalDisabilitySeeder extends Seeder
         PhysicalDisability::query()->truncate();
 
         PhysicalDisability::insert([
-            array('id' => '1', 'code' => '', 'title' => 'চাক্ষুষ অক্ষমতা', 'title_en' => 'Visual Disabilities', 'row_status' => '1', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
-            array('id' => '2', 'code' => '', 'title' => 'শ্রবণ প্রতিবন্ধী', 'title_en' => 'Hearing Disabilities', 'row_status' => '1', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
-            array('id' => '3', 'code' => '', 'title' => 'মানসিক স্বাস্থ্য অক্ষমতা', 'title_en' => 'Mental Health Disabilities', 'row_status' => '1', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
-            array('id' => '4', 'code' => '', 'title' => 'বুদ্ধিবৃত্তিক অক্ষমতা', 'title_en' => 'Intellectual Disabilities', 'row_status' => '1', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
-            array('id' => '5', 'code' => '', 'title' => 'সামাজিক প্রতিবন্ধী', 'title_en' => 'Social Disabilities', 'row_status' => '1', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL)
+            array('id' => '1', 'code' => 'Visual_Dis', 'title' => 'চাক্ষুষ অক্ষমতা', 'title_en' => 'Visual Disabilities', 'row_status' => '1', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
+            array('id' => '2', 'code' => 'Hearing_Dis', 'title' => 'শ্রবণ প্রতিবন্ধী', 'title_en' => 'Hearing Disabilities', 'row_status' => '1', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
+            array('id' => '3', 'code' => 'Mental_H_Dis', 'title' => 'মানসিক স্বাস্থ্য অক্ষমতা', 'title_en' => 'Mental Health Disabilities', 'row_status' => '1', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
+            array('id' => '4', 'code' => 'Intellectual_Dis', 'title' => 'বুদ্ধিবৃত্তিক অক্ষমতা', 'title_en' => 'Intellectual Disabilities', 'row_status' => '1', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
+            array('id' => '5', 'code' => 'Social_Dis', 'title' => 'সামাজিক প্রতিবন্ধী', 'title_en' => 'Social Disabilities', 'row_status' => '1', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL)
         ]);
 
         Schema::enableForeignKeyConstraints();
