192aa8871bc86b73c762151611528a24eecedb3e
ASM Mahmudul Hasan <asmmahmud@gmail.com>
2022-02-23 17:20:47 +0600

    Seeder File Updated

--- database/seeders/EduBoardSeeder.php
+++ database/seeders/EduBoardSeeder.php
@@ -20,16 +20,16 @@ class EduBoardSeeder extends Seeder
         DB::table('edu_boards')->truncate();
 
         $boardName = [
-            array('id' => '1', 'code' => '', 'title_en' => 'Barisal', 'title' => 'বরিশাল', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
-            array('id' => '2', 'code' => '', 'title_en' => 'Chittagong', 'title' => 'চট্টগ্রাম', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
-            array('id' => '3', 'code' => '', 'title_en' => 'Comilla', 'title' => 'কুমিল্লা', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
-            array('id' => '4', 'code' => '', 'title_en' => 'Dhaka', 'title' => 'ঢাকা', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
-            array('id' => '5', 'code' => '', 'title_en' => 'Dinajpur', 'title' => 'দিনাজপুর', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
-            array('id' => '6', 'code' => '', 'title_en' => 'Jessore', 'title' => 'যশোর', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
-            array('id' => '7', 'code' => '', 'title_en' => 'Mymensingh', 'title' => 'ময়মনসিংহ', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
-            array('id' => '8', 'code' => '', 'title_en' => 'Rajshahi', 'title' => 'রাজশাহী', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
-            array('id' => '9', 'code' => '', 'title_en' => 'Sylhet', 'title' => 'সিলেট', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
-            array('id' => '10', 'code' => '', 'title_en' => 'National University', 'title' => 'National University', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL)
+            array('id' => '1', 'code' => 'Barisal', 'title_en' => 'Barisal', 'title' => 'বরিশাল', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
+            array('id' => '2', 'code' => 'Chittagong', 'title_en' => 'Chittagong', 'title' => 'চট্টগ্রাম', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
+            array('id' => '3', 'code' => 'Comilla', 'title_en' => 'Comilla', 'title' => 'কুমিল্লা', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
+            array('id' => '4', 'code' => 'Dhaka', 'title_en' => 'Dhaka', 'title' => 'ঢাকা', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
+            array('id' => '5', 'code' => 'Dinajpur', 'title_en' => 'Dinajpur', 'title' => 'দিনাজপুর', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
+            array('id' => '6', 'code' => 'Jessore', 'title_en' => 'Jessore', 'title' => 'যশোর', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
+            array('id' => '7', 'code' => 'Mymensingh', 'title_en' => 'Mymensingh', 'title' => 'ময়মনসিংহ', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
+            array('id' => '8', 'code' => 'Rajshahi', 'title_en' => 'Rajshahi', 'title' => 'রাজশাহী', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
+            array('id' => '9', 'code' => 'Sylhet', 'title_en' => 'Sylhet', 'title' => 'সিলেট', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL),
+            array('id' => '10', 'code' => 'NU', 'title_en' => 'National University', 'title' => 'National University', 'created_at' => NULL, 'updated_at' => NULL, 'deleted_at' => NULL)
         ];
 
         DB::table('edu_boards')->insert($boardName);
